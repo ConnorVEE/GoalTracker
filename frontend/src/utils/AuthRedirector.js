373541c07a99d3cdcb0a79ecaac04e630e0f2ad1
Connor VanTress <11cvantress@gmail.com>
2025-09-27 17:46:18 -0400

    route mis-named

--- frontend/src/utils/AuthRedirector.js
+++ frontend/src/utils/AuthRedirector.js
@@ -10,8 +10,8 @@ const AuthRedirector = () => {
         // Redirect only after loading is done and the user is NOT authenticated
         if (!loading && !isAuthenticated) {
             // Prevent navigation if we are already on the login page
-            if (window.location.pathname !== '/login') {
-                navigate('/login');
+            if (window.location.pathname !== '/') {
+                navigate('/');
             }
         }
     }, [isAuthenticated, loading, navigate]);
